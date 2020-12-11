2b52f50127142640af233c5a85245d7be460d574
kostyrko <mkostyrko@gmail.com>
2020-12-11 18:28:08 +0100

    Add locales/new-features

--- frontend-project/src/pages/features/new/locales/en-US.ts
+++ frontend-project/src/pages/features/new/locales/en-US.ts
@@ -3,7 +3,9 @@ export default {
   'feature-new.form.name.label': 'Name',
   'feature-new.form.name.placeholder': 'Feature name',
   'feature-new.form.name.required-error': 'Name is required',
-  'feature-new.form.options.label': 'Feature options',
-  'feature-new.form.options.placeholder': 'Options',
+  'feature-new.form.featureoption.label': 'Feature option',
+  'feature-new.form.featureoption.placeholder': 'Feature option',
+  'feature-new.form.featureoption.required-error': 'Feature option is required',
+  'feature-new.form.featureoption.button': 'Add feature option',
   'feature-new.page-header-content': 'You can add new feature here.',
 };
--- frontend-project/src/pages/features/new/locales/pl-PL.ts
+++ frontend-project/src/pages/features/new/locales/pl-PL.ts
@@ -3,7 +3,9 @@ export default {
   'feature-new.form.name.label': 'Nazwa',
   'feature-new.form.name.placeholder': 'Nazwa cechy',
   'feature-new.form.name.required-error': 'Nazwa jest wymagana',
-  'feature-new.form.options.label': 'Opcje cechy',
-  'feature-new.form.options.placeholder': 'Opcje...',
-  'feature-new.page-header-content': 'Możesz dodać tutaj nową cechę.',
+  'feature-new.form.featureoption.label': 'Opcja cechy',
+  'feature-new.form.featureoption.placeholder': 'Opcja cechy',
+  'feature-new.form.featureoption.required-error': 'Opcja cechy jest wymagana',
+  'feature-new.form.featureoption.button': 'Dodaj opcję cechy',
+  'feature-new.page-header-content': 'Tutaj możesz dodać nową cechę.',
 };
