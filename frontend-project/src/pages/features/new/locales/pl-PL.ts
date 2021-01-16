b9edbe9727310c69ab555544522ac31685d8bd4b
kostyrko <mkostyrko@gmail.com>
2021-01-16 18:20:04 +0100

    set simple validation -> feature options required

--- frontend-project/src/pages/features/new/locales/pl-PL.ts
+++ frontend-project/src/pages/features/new/locales/pl-PL.ts
@@ -1,9 +1,13 @@
 export default {
   'feature-new.form.save.label': 'Zapisz',
-  'feature-new.form.name.label': 'Nazwa',
+  'feature-new.form.name.label': 'Nazwa cechy',
   'feature-new.form.name.placeholder': 'Nazwa cechy',
   'feature-new.form.name.required-error': 'Nazwa jest wymagana',
   'feature-new.form.options.label': 'Opcje cechy',
   'feature-new.form.options.placeholder': 'Opcje...',
   'feature-new.page-header-content': 'Możesz dodać tutaj nową cechę.',
+  'feature-new.form.featureoption.button': 'Dodaj opcję cechy',
+  'feature-new.form.featureoption.label': 'Opcja cechy',
+  'feature-new.form.featureoption.placeholder': 'Opcja cechy',
+  'feature-new.form.featureoption.required-error': 'Opcja cechy jest wymagana',
 };
